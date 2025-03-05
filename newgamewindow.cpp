aef5cf9126d18b16f968e7154eb430168415c4c7
Maxim Surin <surin.maks@yandex.ru>
2025-03-05 18:30:11 +0300

    Первый релиз

---
+++ newgamewindow.cpp
@@ -0,0 +1,40 @@
+#include "newgamewindow.h"
+#include "ui_newgamewindow.h"
+#include <QMessageBox>
+
+newGameWindow::newGameWindow(QWidget *parent) : QDialog(parent) , ui(new Ui::newGameWindow){
+    ui->setupUi(this);
+
+    QIntValidator *intValidator = new QIntValidator(kMinValueFieldSize, kMaxValueFiledSize, this);
+    ui->le_horizontSize->setValidator(intValidator);
+    ui->le_vetricalSize->setValidator(intValidator);
+
+    connect(ui->bt_OK, &QPushButton::clicked, this, &newGameWindow::BtPressOK);
+    connect(ui->bt_Cancel, &QPushButton::clicked, this, &newGameWindow::close);
+}
+
+newGameWindow::~newGameWindow(){
+    delete ui;
+}
+
+void newGameWindow::BtPressOK(){
+    if(CheckFieldSize()){
+        emit send_playing_field_size(ui->le_horizontSize->text().toUInt(), ui->le_vetricalSize->text().toUInt(), ui->le_userName->text());
+        this->close();
+    }
+    else{
+        QMessageBox msq;
+        msq.warning(this, "Предупрежение", "Размеры игрового поля не могут быть меньше " + QString::number(kMinValueFieldSize));
+    }
+
+
+}
+
+bool newGameWindow::CheckFieldSize() const {
+    if( (ui->le_horizontSize->text().toUInt() >= kMinValueFieldSize) && (ui->le_vetricalSize->text().toUInt() >= kMinValueFieldSize) ){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
---
+++ newgamewindow.h
@@ -0,0 +1,33 @@
+#ifndef NEWGAMEWINDOW_H
+#define NEWGAMEWINDOW_H
+
+#include <QDialog>
+#include <QIntValidator>
+
+namespace Ui {
+class newGameWindow;
+}
+
+class newGameWindow : public QDialog{
+    Q_OBJECT
+
+    public:
+        explicit newGameWindow(QWidget *parent = nullptr);
+        ~newGameWindow();
+
+    private:
+        Ui::newGameWindow *ui;
+        // void setBackgroundColor();
+        const uint kMinValueFieldSize {3};
+        const uint kMaxValueFiledSize {99};
+        bool CheckFieldSize() const;
+
+    private slots:
+        void BtPressOK();//Слот для кнопки ОК
+
+    signals:
+        void send_playing_field_size(uint length, uint width, QString name);//отправка размера игрового поля
+
+};
+
+#endif // NEWGAMEWINDOW_H

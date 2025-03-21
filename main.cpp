208d0e903dbacdf25c6a315188c83b23013e2c0e
Maxim Surin <surin.maks@yandex.ru>
2025-03-21 21:48:08 +0300

    Последние обновления

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
+#include <QApplication>
+#include <QMessageBox>
 #include "mainwindow.h"
 
-#include <QApplication>
+int main(int argc, char *argv[]) {
+	QApplication a(argc, argv);
+	MainWindow window;
+	try {
+		MainWindow::connect(&window, &MainWindow::game_is_on,
+						&MainWindow::CreateGameField);
+		window.CreateMenu();
+		window.show();
+	} catch (...) {
+		const QMessageBox msq;
+		msq.critical(nullptr, "Критическая ошибка", "Обратитесь к разработчику!");
+		QApplication::closeAllWindows();
+	}
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    MainWindow::connect(&w, &MainWindow::game_is_on, &MainWindow::CreateGameField);
-    w.CreateMenu();
-    w.show();
-    return a.exec();
+	return a.exec();
 }
